b8a2aaf491b68977e11f80dec47d8ddb28c8cb9b
ankugomkar13 <120354060+ankugomkar13@users.noreply.github.com>
2024-01-08 22:15:46 +0530

    Add files via upload

---
+++ java_datatype/src/java/sample/data/Data.java
@@ -0,0 +1,13 @@
+package java.sample.data;
+public class Data 
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=10;
+		int sum=0;
+		sum=a+b;
+		System.out.println(sum);
+		
+	}
+}
